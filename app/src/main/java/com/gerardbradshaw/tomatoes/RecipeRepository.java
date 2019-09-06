b5a2a692e0a6441f743b52e972e517229cd5f72d
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2019-09-06 11:42:14 -0700

    Added repository methods for RecipeSummary

--- app/src/main/java/com/gerardbradshaw/tomatoes/RecipeRepository.java
+++ app/src/main/java/com/gerardbradshaw/tomatoes/RecipeRepository.java
@@ -1,6 +1,7 @@
 package com.gerardbradshaw.tomatoes;
 
 import android.app.Application;
+import android.os.AsyncTask;
 
 import androidx.lifecycle.LiveData;
 
@@ -10,8 +11,6 @@ import com.gerardbradshaw.tomatoes.daos.RecipeIngredientDao;
 import com.gerardbradshaw.tomatoes.daos.RecipeStepDao;
 import com.gerardbradshaw.tomatoes.entities.RecipeSummary;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class RecipeRepository {
@@ -25,11 +24,14 @@ public class RecipeRepository {
 
   private LiveData<List<RecipeSummary>> recipeSummaryList;
 
-  //TODO add live data
-
 
   // - - - - - - - - - - - - - - - Constructor - - - - - - - - - - - - - - -
 
+  /**
+   * Initializes the database
+   *
+   * @param application: Application context wrapper
+   */
   RecipeRepository(Application application) {
 
     // Initialize the database and get a handle on the DAOs
@@ -50,5 +52,99 @@ public class RecipeRepository {
     return recipeSummaryList;
   }
 
+  /**
+   * Wrapper method for the InsertRecipeAsyncTask class, which calls the insertRecipeSummary method
+   * for the RecipeSummaryDao.
+   *
+   * @param recipeSummary the RecipeSummary to be inserted.
+   */
+  public void insertRecipeSummary(RecipeSummary recipeSummary) {
+    new insertRecipeSummaryAsync(recipeSummaryDao).execute(recipeSummary);
+  }
+
+  /**
+   * Wrapper method for the DeleteRecipeAsyncTask class, which calls the deleteRecipeSummary method
+   * for the RecipeSummaryDao.
+   *
+   * @param recipeSummary the RecipeSummary to be deleted.
+   */
+  public void deleteRecipeSummary(RecipeSummary recipeSummary) {
+    new deleteRecipeSummaryAsync(recipeSummaryDao).execute(recipeSummary);
+  }
+
+  /**
+   * Wrapper method for the updateRecipeAsyncTask class, which calls the updateRecipeSummary method
+   * for the RecipeSummaryDao.
+   *
+   * @param recipeSummary the RecipeSummary to be updated.
+   */
+  public void updateRecipeSummary(RecipeSummary recipeSummary) {
+    new updateRecipeSummaryAsync(recipeSummaryDao).execute(recipeSummary);
+  }
+
+
+  // - - - - - - - - - - - - - - - AsyncTasks for wrapper methods - - - - - - - - - - - - - - -
+
+  /**
+   * AsyncTask for insertRecipeSummary.
+   */
+  private static class insertRecipeSummaryAsync extends AsyncTask<RecipeSummary, Void, Void> {
+
+    // Member variable for the the DAO
+    private RecipeSummaryDao dao;
+
+    // Constructor
+    insertRecipeSummaryAsync(RecipeSummaryDao dao) {
+      this.dao = dao;
+    }
+
+    // Background task
+    @Override
+    protected Void doInBackground(final RecipeSummary... recipeSummaries) {
+      dao.insertRecipe(recipeSummaries[0]);
+      return null;
+    }
+  }
+
+  /**
+   * AsyncTask for deleteRecipeSummary.
+   */
+  private static class deleteRecipeSummaryAsync extends AsyncTask<RecipeSummary, Void, Void> {
+
+    // Member variable
+    private RecipeSummaryDao dao;
+
+    // Constructor
+    deleteRecipeSummaryAsync(RecipeSummaryDao dao) {
+      this.dao = dao;
+    }
+
+    // Background task
+    @Override
+    protected Void doInBackground(RecipeSummary... recipeSummaries) {
+      dao.deleteRecipe(recipeSummaries[0]);
+      return null;
+    }
+  }
+
+  /**
+   * AsyncTask for updateRecipeSummary.
+   */
+  private static class updateRecipeSummaryAsync extends AsyncTask<RecipeSummary, Void, Void> {
+
+    // Member variable for the dao
+    private RecipeSummaryDao dao;
+
+    // Constructor
+    updateRecipeSummaryAsync(RecipeSummaryDao dao) {
+      this.dao = dao;
+    }
+
+    @Override
+    protected Void doInBackground(RecipeSummary... recipeSummaries) {
+      dao.updateRecipe(recipeSummaries[0]);
+      return null;
+    }
+  }
 
 }
