09bed2e12d1dd6575a7c51b44d3d48977023df43
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2019-09-07 11:32:46 -0700

    Updated layouts
    Started adding dynamic elements

--- app/src/main/java/com/gerardbradshaw/tomatoes/AddRecipeActivity.java
+++ app/src/main/java/com/gerardbradshaw/tomatoes/AddRecipeActivity.java
@@ -3,6 +3,9 @@ package com.gerardbradshaw.tomatoes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class AddRecipeActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class AddRecipeActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_add_recipe);
+
+    // Locate the ingredient layout
+    LinearLayout ingredientLayout = findViewById(R.id.addRecipeActivity_addIngredientLayout);
+
+    // Add the first field
+    EditText nameInput = new EditText(this);
+
+    // Define the parameters of the view
+    LinearLayout.LayoutParams nameInputParams = new LinearLayout.LayoutParams(
+        LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+    // Set parameters to nameInput
+    nameInput.setLayoutParams(nameInputParams);
+
+    // Set other attributes
+    nameInput.setHint(getString(R.string.addRecipeActivity_addStepLayout_stepInput_hint));
+
+
   }
 }
