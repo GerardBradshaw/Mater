ed79e2499318155cc206fc4c46e4a667580b18fb
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2019-09-06 23:24:08 -0700

    Created AddRecipe layout

---
+++ app/src/main/java/com/gerardbradshaw/tomatoes/AddRecipeActivity.java
@@ -0,0 +1,14 @@
+package com.gerardbradshaw.tomatoes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddRecipeActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_add_recipe);
+  }
+}
