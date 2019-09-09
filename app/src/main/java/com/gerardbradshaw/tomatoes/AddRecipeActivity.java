5613e5fdfae704f4022f17e72715122243e8f0f0
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2019-09-08 21:54:49 -0700

    Save button functionality
    Button now creates a Recipe object ready to save to the database

--- app/src/main/java/com/gerardbradshaw/tomatoes/AddRecipeActivity.java
+++ app/src/main/java/com/gerardbradshaw/tomatoes/AddRecipeActivity.java
@@ -13,6 +13,10 @@ import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.gerardbradshaw.tomatoes.holders.RecipeHolder;
+import com.gerardbradshaw.tomatoes.holders.RecipeIngredientHolder;
+import com.gerardbradshaw.tomatoes.room.entities.Ingredient;
+import com.gerardbradshaw.tomatoes.room.entities.RecipeIngredient;
 import com.gerardbradshaw.tomatoes.viewholders.AddIngredientViewHolder;
 import com.gerardbradshaw.tomatoes.viewholders.AddStepViewHolder;
 
@@ -23,11 +27,17 @@ public class AddRecipeActivity extends AppCompatActivity {
 
   // - - - - - - - - - - - - - - - Member variables - - - - - - - - - - - - - - -
 
+  // Default layout views
   private Button addIngredientButton;
   private Button addStepButton;
+  private Button saveButton;
+  private EditText titleInput;
+  private EditText descriptionInput;
+
+  // Dynamically added view references
   private List<AddIngredientViewHolder> ingredientViewHolders;
   private List<AddStepViewHolder> stepViewHolders;
-  private EditText titleInput;
+
 
 
 
@@ -41,7 +51,9 @@ public class AddRecipeActivity extends AppCompatActivity {
     // Get a handle on the views
     addIngredientButton = findViewById(R.id.addRecipeActivity_addIngredientButton);
     addStepButton = findViewById(R.id.addRecipeActivity_addStepButton);
+    saveButton = findViewById(R.id.addRecipeActivity_saveButton);
     titleInput = findViewById(R.id.addRecipeActivity_titleInput);
+    descriptionInput = findViewById(R.id.addRecipeActivity_descriptionInput);
 
     // Initialize the view holders
     ingredientViewHolders = new ArrayList<>();
@@ -62,6 +74,15 @@ public class AddRecipeActivity extends AppCompatActivity {
         addStep();
       }
     });
+
+    // Set listener for saveButton
+    saveButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        saveRecipe();
+      }
+    });
+
   }
 
 
@@ -140,5 +161,48 @@ public class AddRecipeActivity extends AppCompatActivity {
 
 
   }
+
+  private void saveRecipe() {
+
+    // Create a RecipeHolder object
+    RecipeHolder recipe = new RecipeHolder();
+
+    // Get the title and description
+    recipe.setTitle(titleInput.getText().toString());
+    recipe.setDescription(descriptionInput.getText().toString());
+
+    // Set up holders for steps and ingredients
+    List<RecipeIngredient> ingredients = new ArrayList<>();
+    List<String> steps = new ArrayList<>();
+
+    // Get the ingredients, amounts, and units from each and add them to the recipe
+    for(AddIngredientViewHolder holder : ingredientViewHolders) {
+
+      // Get user input
+      String name = holder.getNameInput().getText().toString();
+      double amount = Double.parseDouble(holder.getNameInput().getText().toString());
+      RecipeIngredientHolder.Unit unit = RecipeIngredientHolder.Unit.NO_UNIT;
+      // TODO implement spinner functionality and retrieval
+
+      // Add a RecipeIngredientHolder to the recipe
+      recipe.addIngredient(new RecipeIngredientHolder(name, amount, unit));
+    }
+
+    // Get the steps from each and add them to the recipe
+    for(AddStepViewHolder holder : stepViewHolders) {
+
+      // Get user input
+      String step = holder.getStep().getText().toString();
+
+      // Add the step to the recipe
+      recipe.addNewStep(step);
+    }
+
+    // TODO save the recipe to the database using a VH
+
+    // TODO add a ProgressBar
+
+  }
+
 
 }
