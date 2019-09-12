7c3878125cd7199d61559b0a4c0123288ef9d9ee
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2019-09-12 10:21:17 -0700

    Added AppBar to detail activity
    Also removed styles.xml (v21) for simplicity

--- app/src/main/java/com/gerardbradshaw/tomatoes/activities/RecipeDetailActivity.java
+++ app/src/main/java/com/gerardbradshaw/tomatoes/activities/RecipeDetailActivity.java
@@ -1,6 +1,7 @@
 package com.gerardbradshaw.tomatoes.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
@@ -52,6 +53,10 @@ public class RecipeDetailActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_recipe_detail);
+
+    // Set up toolbar
+    Toolbar toolbar = findViewById(R.id.recipeDetail_toolbar);
+    setSupportActionBar(toolbar);
 
     // Initialize the ViewModel
     viewModel = ViewModelProviders.of(this).get(RecipeDetailViewModel.class);
