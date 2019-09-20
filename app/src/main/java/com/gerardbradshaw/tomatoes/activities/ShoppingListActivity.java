e5f4b305f63e93af398ee4658bb3f6f60ba058b6
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2019-09-20 15:03:04 -0700

    Created ShoppingList activity

---
+++ app/src/main/java/com/gerardbradshaw/tomatoes/activities/ShoppingListActivity.java
@@ -0,0 +1,16 @@
+package com.gerardbradshaw.tomatoes.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gerardbradshaw.tomatoes.R;
+
+public class ShoppingListActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_shopping_list);
+  }
+}
