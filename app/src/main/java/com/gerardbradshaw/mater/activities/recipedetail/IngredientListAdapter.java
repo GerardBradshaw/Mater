acfe78671a19194b5dd47ef5c0c05761cff24d0e
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2019-09-27 11:42:23 -0700

    Completed ViewHolder and added annotations

--- app/src/main/java/com/gerardbradshaw/mater/activities/recipedetail/IngredientListAdapter.java
+++ app/src/main/java/com/gerardbradshaw/mater/activities/recipedetail/IngredientListAdapter.java
@@ -2,6 +2,7 @@ package com.gerardbradshaw.mater.activities.recipedetail;
 
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,17 +13,40 @@ import com.gerardbradshaw.mater.R;
 import com.gerardbradshaw.mater.activities.main.RecipeListAdapter;
 
 public class IngredientListAdapter extends RecyclerView.Adapter<IngredientListAdapter.IngredientViewHolder> {
+
+  /**
+   * Called when RecyclerView needs a new ViewHolder of the given type to represent an item.
+   *
+   * @param parent the ViewGroup into which the new View will be added after it is bound to an adapter position.
+   * @param viewType the view type of the new View
+   * @return RecipeViewHolder: The inflated view.
+   */
   @NonNull
   @Override
   public IngredientViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
     return null;
   }
 
+  /**
+   * Called by RecyclerView to display the data at the specified position and set up input
+   * listeners. This method updates the contents of the itemView to reflect the item at the given
+   * position.
+   *
+   * @param holder the ViewHolder which should be updated to represent the contents of the item at the given position in the data set
+   * @param position the position of the item within the adapter's data set
+   */
   @Override
   public void onBindViewHolder(@NonNull IngredientViewHolder holder, int position) {
 
   }
 
+  /**
+   * Adapter method to return the number of items to be displayed in the RecyclerView.
+   * This method is called many times, and when it is first called, this.summaryList has not
+   * been updated. This means, initially, it will return zero.
+   *
+   * @return the number of items
+   */
   @Override
   public int getItemCount() {
     return 0;
@@ -34,7 +58,9 @@ public class IngredientListAdapter extends RecyclerView.Adapter<IngredientListAd
   class IngredientViewHolder extends RecyclerView.ViewHolder {
 
     // Member variables
-    // TODO views
+    private final CheckBox checkBox;
+    private final TextView quantity;
+    private final TextView name;
     final IngredientListAdapter adapter;
 
     // Constructor
@@ -42,7 +68,9 @@ public class IngredientListAdapter extends RecyclerView.Adapter<IngredientListAd
       super(itemView);
 
       // Initialize the views and adapter.
-      // TODO views
+      checkBox = itemView.findViewById(R.id.ingredientView_checkBox);
+      quantity = itemView.findViewById(R.id.ingredientView_amount);
+      name = itemView.findViewById(R.id.ingredientView_name);
       this.adapter = adapter;
     }
   }
