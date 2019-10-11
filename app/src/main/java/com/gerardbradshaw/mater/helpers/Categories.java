49fcfbef805c5c060b31d32c10c558012604386f
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2019-10-10 17:18:04 -0700

    WIP removing "Item" from app
    Created Categories class for simple reference to ingredient categories

---
+++ app/src/main/java/com/gerardbradshaw/mater/helpers/Categories.java
@@ -0,0 +1,21 @@
+package com.gerardbradshaw.mater.helpers;
+
+public class Categories {
+
+  public enum Category {
+    FRESH_FRUIT_AND_VEG,
+    BAKERY,
+    CANNED_GOODS,
+    INTERNATIONAL_FOOD,
+    COLD_FOOD,
+    OILS_AND_CONDIMENTS,
+    BAKING,
+    SPICES,
+    KITCHEN_GOODS,
+    PERSONAL_HYGIENE,
+    CLEANING_SUPPLIES,
+    SUPPLEMENTS_AND_MEDICINES,
+    MISCELLANEOUS;
+  }
+
+}
