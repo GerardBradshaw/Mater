884005571b515b4b65c33121db2c80ac0f5bfe53
Gerard Bradshaw <gerard.bradshaw@gmail.com>
2019-10-28 15:41:08 -0700

    WIP creating alarm receiver class
    Removed default implementation of onReceive().

--- app/src/main/java/com/gerardbradshaw/mater/activities/main/AlarmReceiver.java
+++ app/src/main/java/com/gerardbradshaw/mater/activities/main/AlarmReceiver.java
@@ -19,9 +19,7 @@ public class AlarmReceiver extends BroadcastReceiver {
 
   @Override
   public void onReceive(Context context, Intent intent) {
-    // TODO: This method is called when the BroadcastReceiver is receiving
-    // an Intent broadcast.
-    throw new UnsupportedOperationException("Not yet implemented");
+    deliverNotification(context);
   }
 
   private void deliverNotification(Context context) {
